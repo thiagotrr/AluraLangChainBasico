e41adfb55deb60a1b51d47d150bb531af0e2ed1c
Thiago Rodrigues <thiago.trrodrigues@gmail.com>
2025-07-24 19:09:53 -0300

    Adicionando prompt template

--- main_langchain.py
+++ main_langchain.py
@@ -1,15 +1,24 @@
 # Case: agente de viagem com LangChain
 import os
 from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
 
 numero_de_dias = 7
 numero_de_criancas = 2
 atividade = "praia"
-prompt = f"Crie um roteiro de viagens de {numero_de_dias} dias, para uma família com {numero_de_criancas} crianças, que gostam de {atividade}"
-print(prompt)
+
+modelo_prompt = PromptTemplate.from_template(
+    "Crie um roteiro de viagens de {dias} dias, para uma família com {criancas} crianças, que gostam de {atividade}"
+)
+
+prompt = modelo_prompt.format(
+    dias=numero_de_dias,
+    criancas=numero_de_criancas,
+    atividade=atividade,
+)
 
 llm = ChatOpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),
+    api_key=os.environ.get("OPENAI_API_KEY"), #Registrado nas variáveis de ambiente do computador.
     model="gpt-4o",
     temperature=0.7,
     )
